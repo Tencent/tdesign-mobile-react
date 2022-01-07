c23c1549d4e9ccd2512d224ac6791303c73c859b
colinfan <colinfan@tencent.com>
2022-01-07 17:47:28 +0800

    fix: 1、联调 tdesign-cli 生成文档；2、同步 tdesign-react 的 eslintrc 配置；3、新增 vscode 配置文件； 4、新增 src/common.ts 文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,76 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
   extends: [
+    'airbnb-base',
+    'prettier', // eslint-config-prettier 处理冲突
     'plugin:react/recommended',
-    'airbnb',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
   ],
   parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react-hooks'],
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
   parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 13,
-    sourceType: 'module',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
   rules: {
+    'no-use-before-define': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/indent': ['off', 2],
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/no-empty-interface': 'error', // codecc
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-unused-vars': 'error', // codecc
+    'import/order': 'error',
+    'import/extensions': 'off',
+    'import/no-named-as-default': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-cycle': 'off', // TODO: turn on this rule later
+    'import/no-unresolved': 'off', // TODO: turn on this rule later
+    'react/display-name': 'off',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react/prop-types': 'off',
+    'max-len': 'off',
+    'no-shadow': 'off',
+    'no-console': 'off',
+    'no-throw-literal': 'off',
+    'no-unused-expressions': 'off',
+    'no-bitwise': 'off',
+    'no-useless-return': 'off',
+    'no-plusplus': [
+      'error',
+      {
+        allowForLoopAfterthoughts: true,
+      },
+    ],
+    'no-continue': 'off',
+    'no-return-assign': 'off',
+    'no-restricted-syntax': 'off',
+    'no-restricted-globals': 'off',
+    'no-unneeded-ternary': 'off',
+    'eol-last': 'error', // codecc
+    'func-names': 'off',
+    'consistent-return': 'off',
+    'default-case': 'off',
   },
 };
--- src/button/index.ts
+++ src/button/index.ts
@@ -2,8 +2,8 @@ import _Button from './Button';
 
 import './style/index.js';
 
-// export type { ButtonProps } from './Button';
-// export * from './type';
+export type { ButtonProps } from './Button';
+export * from './type';
 
 export const Button = _Button;
 export default Button;
