0e1b93513930134cf14eb132c563c17d6c1a1706
stormzhang <stormzhang@tencent.com>
2022-02-19 17:26:55 +0800

    feat(switch): complete Switch Component

--- src/switch/Switch.tsx
+++ src/switch/Switch.tsx
@@ -1,15 +1,67 @@
-import React from 'react';
+import React, { forwardRef, useEffect, useState } from 'react';
+import classNames from 'classnames';
 import { TdSwitchProps } from './type';
+import { StyledProps } from '../common';
 import useConfig from '../_util/useConfig';
 
-export interface SwitchProps extends TdSwitchProps {}
+interface SwitchProps extends TdSwitchProps, StyledProps {}
 
-const Switch: React.FC<SwitchProps> = ({ children }) => {
-  const { classPrefix } = useConfig();
+const Switch = forwardRef<HTMLButtonElement, SwitchProps>(
+  ({ customValue = [true, false], value, defaultValue, label, onChange, disabled, className, style }, ref) => {
+    const { classPrefix } = useConfig();
 
-  console.log('classPrefix ===> ', classPrefix);
+    const [activeValue, inactiveValue] = customValue;
 
-  return <div>{children}</div>;
-};
+    const [checked, setChecked] = useState<boolean>(defaultValue === activeValue);
+
+    const onInternalClick = () => {
+      if (disabled) return;
+
+      setChecked(!checked);
+
+      onChange?.(!checked ? activeValue : inactiveValue);
+    };
+
+    const renderSwitchText = (checked: boolean, label: SwitchProps['label']) => {
+      if (typeof label === 'function') {
+        return label({ value: checked ? activeValue : inactiveValue });
+      }
+
+      if (typeof label === 'string') return label;
+
+      if (Array.isArray(label)) {
+        const [activeContent, inactiveContent] = label;
+        const content = checked ? activeContent : inactiveContent;
+
+        if (typeof content === 'function') return content();
+
+        return content;
+      }
+
+      return null;
+    };
+
+    useEffect(() => {
+      if (typeof value !== 'undefined') {
+        if (Array.isArray(customValue) && !customValue.includes(value)) {
+          throw `${value} is not in customValue: ${JSON.stringify(customValue)}`;
+        }
+        setChecked(value === customValue[0]);
+      }
+    }, [value, customValue]);
+
+    const switchClassName = classNames(`${classPrefix}-switch`, className, {
+      [`${classPrefix}-is-checked`]: checked,
+      [`${classPrefix}-is-disabled`]: disabled,
+    });
+
+    return (
+      <button ref={ref} className={switchClassName} style={style} onClick={onInternalClick}>
+        <span className={`${classPrefix}-switch__text`}>{renderSwitchText(checked, label)}</span>
+        <span className={`${classPrefix}-switch__node`}></span>
+      </button>
+    );
+  },
+);
 
 export default Switch;
--- src/switch/_example/index.jsx
+++ src/switch/_example/index.jsx
@@ -1,5 +1,22 @@
+import { useState } from 'react';
 import { Switch } from 'tdesign-mobile-react';
 
 export default function () {
-  return <Switch>Switch test</Switch>;
+  const [value, setValue] = useState('开');
+
+  return (
+    <Switch
+      customValue={['开', '关']}
+      defaultValue={'开'}
+      label={({ value }) => {
+        return value;
+      }}
+      value={value}
+      onChange={(value) => {
+        // @ts-ignore
+        setValue(value);
+        console.log('value ===> ', value);
+      }}
+    />
+  );
 }
