d0949cfa5ee891ece8a6f40b717eaad71c92423e
stormzhang <stormzhang@tencent.com>
2022-02-21 19:40:18 +0800

    fix(moblie.config.js and button.md): revert unrelate change

--- docs/mobile/mobile.config.js
+++ docs/mobile/mobile.config.js
@@ -3,37 +3,31 @@ export default {
     {
       title: 'Button 按钮',
       name: 'button',
-      path: '/button',
       component: () => import('tdesign-mobile-react/button/_example/base.jsx'),
     },
     {
       title: 'Divider 分割线',
       name: 'divider',
-      path: '/divider',
       component: () => import('tdesign-mobile-react/divider/_example/base.jsx'),
     },
     {
       title: 'Grid 宫格',
       name: 'grid',
-      path: '/grid',
       component: () => import('tdesign-mobile-react/grid/_example/base.jsx'),
     },
     {
       title: 'Popup 弹出层',
       name: 'popup',
-      path: '/popup',
       component: () => import('tdesign-mobile-react/popup/_example/base.jsx'),
     },
     {
       title: 'Radio 单选框',
       name: 'radio',
-      path: '/radio',
       component: () => import('tdesign-mobile-react/radio/_example/index.jsx'),
     },
     {
       title: 'Badge 徽标',
       name: 'badge',
-      path: '/badge',
       component: () => import('tdesign-mobile-react/badge/_example/base.jsx'),
     },
     {
