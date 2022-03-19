cb97b6ab60490f0ac5fe01ce85c6d9c0ca187dd9
samueljiang <samueljiang@tencent.com>
2022-03-19 20:35:29 +0800

    refactor(countdown): countdown组件优化defaultProps写法，使用widthNativeProps给组件container容器元素加上native属性

---
+++ src/_util/widthNativeProps.ts
@@ -0,0 +1,30 @@
+import React, { CSSProperties, ReactElement } from 'react';
+import cls from 'classnames';
+import has from 'lodash/has';
+
+export interface NativeProps<S extends string = never> {
+  className?: string;
+  style?: CSSProperties & Partial<Record<S, string>>;
+}
+
+export default function withNativeProps<P extends NativeProps>(props: P, element: ReactElement) {
+  const p = {
+    ...element.props,
+  };
+  if (props.className) {
+    p.className = cls(element.props.className, props.className);
+  }
+  if (props.style) {
+    p.style = {
+      ...p.style,
+      ...props.style,
+    };
+  }
+  for (const key in props) {
+    if (!has(props, key)) continue;
+    if (key.startsWith('data-') || key.startsWith('aria-')) {
+      p[key] = props[key];
+    }
+  }
+  return React.cloneElement(element, p);
+}
