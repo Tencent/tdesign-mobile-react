6d72d343d6753d52fa8e32ddfc8c243881b7d02b
leejimqiu <leejimqiu@gmail.com>
2022-05-07 11:33:06 +0800

    fix(upload): update demo

--- src/upload/_example/base.jsx
+++ src/upload/_example/base.jsx
@@ -1,16 +1,25 @@
 // @ts-ignore
 import React from 'react';
-import { Upload, Cell } from 'tdesign-mobile-react';
+import { Upload } from 'tdesign-mobile-react';
+
+import './style/index.less';
 
 export default () => {
+  const files = [
+    {
+      url: 'https://tdesign.gtimg.com/site/upload1.png',
+      name: 'uploaded1.png',
+      type: 'image',
+    },
+  ];
   return (
-    <>
-      <Cell
-        title="上传图片"
-        description={
-          <Upload accept="image/png" action="//service-bv448zsw-1257786608.gz.apigw.tencentcs.com/api/upload-demo" />
-        }
+    <div className="demo-section">
+      <h3 className="demo-section__title">上传图片</h3>
+      <Upload
+        accept="image/png"
+        files={files}
+        action="//service-bv448zsw-1257786608.gz.apigw.tencentcs.com/api/upload-demo"
       />
-    </>
+    </div>
   );
 };
