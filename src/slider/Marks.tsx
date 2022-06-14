675be432fcfb46978e8043c662aaf0c1b0908c5a
leejimqiu <leejimqiu@gmail.com>
2022-06-14 17:01:01 +0800

    fix(slider): using correct style

--- src/slider/Marks.tsx
+++ src/slider/Marks.tsx
@@ -1,7 +1,8 @@
-import isArray from "lodash/isArray";
-import React, { FC } from "react";
-import useConfig from "tdesign-mobile-react/_util/useConfig";
-import { SliderMarks } from "./type";
+import isArray from 'lodash/isArray';
+import React, { FC } from 'react';
+import cls from 'classnames';
+import useConfig from 'tdesign-mobile-react/_util/useConfig';
+import { SliderMarks } from './type';
 
 interface MarkProps {
   range: boolean;
@@ -20,37 +21,36 @@ const Marks: FC<MarkProps> = (props) => {
   const name = `${classPrefix}-slider`;
 
   // 渲染单个刻度
-  const renderMark = (item, index, renderIndex = false) => (
-    <div
-      className={`${name}__mark-text`}
-      key={index}
-      style={{
-        left: `${renderIndex ? index : item}%`,
-      }}
-    >
-      {item}
-    </div>
-  );
+  const renderMark = (item, index, renderIndex = false) => {
+    const textClass = cls({
+      [`${name}__mark-text`]: true,
+      [`${classPrefix}-is-active`]: value[1] > index,
+    });
+
+    return (
+      <div
+        className={textClass}
+        key={index}
+        style={{
+          left: `${renderIndex ? index : item}%`,
+        }}
+      >
+        {item}
+      </div>
+    );
+  };
 
   if (range || (marks && isArray(marks))) {
-    return (
-      <div className={`${name}__mark`}>
-        {value.map((item, index) => renderMark(item, index))}
-      </div>
-    );
+    return <div className={`${name}__mark`}>{value.map((item, index) => renderMark(item, index))}</div>;
   }
   if (marks && !isArray(marks)) {
-    return (
-      <div className={`${name}__mark`}>
-        {Object.keys(marks).map((key) => renderMark(marks[key], key, true))}
-      </div>
-    );
+    return <div className={`${name}__mark`}>{Object.keys(marks).map((key) => renderMark(marks[key], key, true))}</div>;
   }
 
   return null;
 };
 
 Marks.defaultProps = defaultProps;
-Marks.displayName = "Marks";
+Marks.displayName = 'Marks';
 
 export default Marks;
