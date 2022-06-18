3c97f5168288900f4b60903775c519bf5d1d3f4b
Peter Qiu <273743552@qq.com>
2022-06-18 12:10:45 +0800

    chore: add icon views

--- site/web/main.jsx
+++ site/web/main.jsx
@@ -9,6 +9,9 @@ import 'tdesign-site-components/lib/styles/style.css';
 import 'tdesign-site-components/lib/styles/prism-theme.less';
 import 'tdesign-site-components/lib/styles/prism-theme-dark.less';
 
+// import icons webcomponents
+import 'tdesign-icons-view';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
