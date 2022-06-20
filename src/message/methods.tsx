038353f80185744a94a9670f5a5f95bce57f8b27
wwlh200 <wwlh200@163.com>
2022-06-20 11:17:46 +0800

    fix(message): message组件修复样式

--- src/message/methods.tsx
+++ src/message/methods.tsx
@@ -6,7 +6,11 @@ import Message from './Message';
 
 const createMessage = (props, theme?: MessageThemeList) => {
   const config = { ...defaultProps, ...props };
+  if(document.getElementById("#t-message")){
+    document.body.removeChild(document.getElementById("#t-message"));
+  }
   const container = document.createElement('div');
+  container.id = "#t-message"
   document.body.appendChild(container);
   ReactDOM.render(<Message {...{ ...config, theme, container }} />, container);
 };
