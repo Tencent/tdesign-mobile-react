c612e08370f7c44148e3491b2168805c098b1846
zwkang <kang95630@gmail.com>
2022-06-22 19:39:24 +0800

    chore(cell): 引入新的默认 props 文件以及文档描述 md 文件

---
+++ src/cell/defaultProps.ts
@@ -0,0 +1,7 @@
+/**
+ * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
+ * */
+
+import { TdCellProps } from './type';
+
+export const cellDefaultProps: TdCellProps = { align: 'middle', arrow: false, bordered: true, required: false };
