006276fad5daed2f2b98e4d7dedfe09a9d1efaa4
anlyyao <anly_yaw@163.com>
2022-06-27 16:28:48 +0800

    fix(checkbox): remove cell related

--- src/checkbox/Checkbox.tsx
+++ src/checkbox/Checkbox.tsx
@@ -11,11 +11,6 @@ export interface CheckProps extends TdCheckboxProps {
   ref: Ref<HTMLLabelElement>;
 }
 
-enum ALIGN {
-  LEFT = 'left',
-  RIGHT = 'right',
-}
-
 export interface CheckContextValue {
   inject: (props: CheckProps) => CheckProps;
 }
@@ -50,21 +45,10 @@ const Checkbox = forwardRef((_props: CheckProps, ref: Ref<HTMLInputElement>) => 
     maxContentRow = 5,
     icon,
     contentDisabled,
+    borderless = false,
   } = props;
   const [internalChecked, setInternalChecked] = useDefault(checked, defaultChecked, onChange);
-  const containerClassName = classNames(
-    `${classPrefix}-cell`,
-    `${classPrefix}-cell--middle`,
-    `${classPrefix}-cell--bordered`,
-  );
-  const alignStyle: CSSProperties = {
-    flexDirection: align === ALIGN.LEFT ? 'row' : 'row-reverse',
-  };
-  const iconClassName = classNames(
-    { [`${classPrefix}-cell__left-icon`]: align === ALIGN.LEFT },
-    { [`${classPrefix}-cell__right-icon`]: align === ALIGN.RIGHT },
-    `${classPrefix}-checkbox__wrap`,
-  );
+
   const checkboxClassName = classNames(`${classPrefix}-checkbox`, {
     [`${classPrefix}-is-checked`]: internalChecked || indeterminate,
     [`${classPrefix}-is-disabled`]: disabled,
@@ -104,38 +88,57 @@ const Checkbox = forwardRef((_props: CheckProps, ref: Ref<HTMLInputElement>) => 
     }
   };
   return (
-    <label className={`${containerClassName}`} style={alignStyle} onClick={handleClick}>
-      <div className={iconClassName}>
-        <div className={checkboxClassName}>
-          <div className={`${classPrefix}-checkbox__content-wrap`}>
-            <span className={`${classPrefix}-checkbox__icon-left`}>
-              <input
-                readOnly={readonly}
-                value={value}
-                ref={ref}
-                type="checkbox"
-                name={name}
-                className={`${classPrefix}-checkbox__original-left`}
-                disabled={disabled}
-                checked={internalChecked}
-                onClick={(e) => e.stopPropagation()}
-                onChange={(e) => {
-                  setInternalChecked(e.currentTarget.checked, { e });
-                }}
-              />
-              {renderIcon()}
+    <>
+      <div className={checkboxClassName}>
+        <div className={`${classPrefix}-checkbox__content-wrap`}>
+         { align ==='left' && <span className={`${classPrefix}-checkbox__icon-left`}>
+            <input
+              readOnly={readonly}
+              value={value}
+              ref={ref}
+              type="checkbox"
+              name={name}
+              className={`${classPrefix}-checkbox__original-left`}
+              disabled={disabled}
+              checked={internalChecked}
+              onClick={(e) => e.stopPropagation()}
+              onChange={(e) => {
+                setInternalChecked(e.currentTarget.checked, { e });
+              }}
+            />
+            {renderIcon()}
+          </span>}
+          <span className={ `${classPrefix}-checkbox__label ${classPrefix}-checkbox__label-left`} >
+            <span style={labelStyle}>
+              {label}
             </span>
-            <span className={`${classPrefix}-checkbox__label-wrap`}></span>
-          </div>
+            <span className={`${classPrefix}-checkbox__description`} style={getLimitRowStyle(maxContentRow)} onClick={handleClick}>
+              {children || content}
+            </span>
+          </span>
+
+          { align ==='right' && <span className={`${classPrefix}-checkbox__icon-right`}>
+            <input
+              readOnly={readonly}
+              value={value}
+              ref={ref}
+              type="checkbox"
+              name={name}
+              className={`${classPrefix}-checkbox__original-right`}
+              disabled={disabled}
+              checked={internalChecked}
+              onClick={(e) => e.stopPropagation()}
+              onChange={(e) => {
+                setInternalChecked(e.currentTarget.checked, { e });
+              }}
+            />
+            {renderIcon()}
+          </span>}
         </div>
+        {/* 下边框 */}
+        { !borderless && <div className={`${classPrefix}-checkbox__border ${classPrefix}-checkbox__border--${align}`}></div>}
       </div>
-      <div className={`${classPrefix}-cell__title`}>
-        <span style={labelStyle}>{label}</span>
-        <div className={`${classPrefix}-cell__description`} style={getLimitRowStyle(maxContentRow)}>
-          {children || content}
-        </div>
-      </div>
-    </label>
+    </>
   );
 });
 
--- src/checkbox/_example/index.jsx
+++ src/checkbox/_example/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './style/index.less';
 import TDemoBlock from '../../../site/mobile/components/DemoBlock';
 import TDemoHeader from '../../../site/mobile/components/DemoHeader';
 import Base from './base';
@@ -14,32 +15,46 @@ export default function CheckboxDemo() {
     <div className="tdesign-mobile-demo">
       <TDemoHeader title="Checkbox 多选框" summary="用于预设的一组选项中执行多项选择，并呈现选择结果。" />
 
-      <TDemoBlock title="01 类型" summary="基础多选框">
-        <Base />
+      <TDemoBlock  title="01 类型" summary="基础多选框">
+        <div className='checkbox-demo'>
+          <Base />
+        </div>
       </TDemoBlock>
 
       <TDemoBlock summary="右侧多选框">
-        <Right />
+        <div className='checkbox-demo'>
+          <Right />
+        </div>
       </TDemoBlock>
 
       <TDemoBlock summary="带全选的多选框">
-        <Group />
+        <div className='checkbox-demo'>
+          <Group />
+        </div>
       </TDemoBlock>
 
       <TDemoBlock summary="限制最多可选数量">
-        <Max />
+        <div className='checkbox-demo'>
+          <Max />
+        </div>
       </TDemoBlock>
 
       <TDemoBlock title="02 状态" summary="多选框禁用态">
-        <Disable />
+        <div className='checkbox-demo'>
+          <Disable />
+        </div>
       </TDemoBlock>
 
       <TDemoBlock summary="多选框半选态">
-        <Indeterminate />
+        <div className='checkbox-demo'>
+          <Indeterminate />
+        </div>
       </TDemoBlock>
 
       <TDemoBlock title="03 特殊类型" summary="自定义图标多选框">
-        <Icon />
+        <div className='checkbox-demo'>
+          <Icon />
+        </div>
       </TDemoBlock>
     </div>
   );
