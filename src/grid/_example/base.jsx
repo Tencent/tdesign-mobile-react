88e668ce426f732a7d40582a9a423b10e3291167
leejimqiu <leejimqiu@gmail.com>
2022-06-28 16:20:46 +0800

    fix(grid): update demo

--- src/grid/_example/base.jsx
+++ src/grid/_example/base.jsx
@@ -13,57 +13,46 @@ export default function Base() {
         title="Grid 宫格"
         summary="用于功能入口布局，将页面或特定区域切分成若干等大的区块，形成若干功能入口。"
       />
-      <TDemoBlock title=" " summary="基础宫格">
-        <div className="grid-demo">
-          <Grid column={5}>
-            <GridItem image={imgUrl} text="标题文字" />
-            <GridItem image={imgUrl} text="标题文字" />
-            <GridItem image={imgUrl} text="标题文字" />
-            <GridItem image={imgUrl} text="标题文字" />
-            <GridItem image={imgUrl} text="最多四字" />
-          </Grid>
-        </div>
-        <div className="grid-demo">
-          <Grid>
-            <GridItem image={imgUrl} text="标题文字" />
-            <GridItem image={imgUrl} text="标题文字" />
-            <GridItem image={imgUrl} text="标题文字" />
-            <GridItem image={imgUrl} text="标题五字内" />
-          </Grid>
-        </div>
-        <div className="grid-demo">
-          <Grid column={3}>
-            <GridItem image={imgUrl} text="标题文字" />
-            <GridItem image={imgUrl} text="标题文字" />
-            <GridItem image={imgUrl} text="标题最多六字" />
-          </Grid>
-        </div>
-        <div className="grid-demo short-width">
-          <Grid column={5}>
-            <GridItem image={imgUrl} text="标题文字" />
-            <GridItem image={imgUrl} text="标题文字" />
-            <GridItem image={imgUrl} text="标题文字" />
-            <GridItem image={imgUrl} text="标题文字" />
-            <GridItem image={imgUrl} text="最多四字" />
-          </Grid>
-        </div>
+      <TDemoBlock title="01 类型" summary="一行三个">
+        <Grid column={3}>
+          <GridItem image={imgUrl} text="标题文字" />
+          <GridItem image={imgUrl} text="标题文字" />
+          <GridItem image={imgUrl} text="标题最多六字" />
+        </Grid>
+      </TDemoBlock>
+      <TDemoBlock summary="一行四个">
+        <Grid>
+          <GridItem image={imgUrl} text="标题文字" />
+          <GridItem image={imgUrl} text="标题文字" />
+          <GridItem image={imgUrl} text="标题文字" />
+          <GridItem image={imgUrl} text="标题五字内" />
+        </Grid>
+      </TDemoBlock>
+      <TDemoBlock summary="一行五个">
+        <Grid column={5}>
+          <GridItem image={imgUrl} text="标题文字" />
+          <GridItem image={imgUrl} text="标题文字" />
+          <GridItem image={imgUrl} text="标题文字" />
+          <GridItem image={imgUrl} text="标题文字" />
+          <GridItem image={imgUrl} text="最多四字" />
+        </Grid>
       </TDemoBlock>
 
-      <TDemoBlock title=" " summary="带说明宫格">
-        <div className="grid-demo">
+      <TDemoBlock title=" " summary="一行二个带说明宫格">
           <Grid column={2}>
             <GridItem image={imgUrl} layout="horizontal" text="标题文字" description="说明文字" />
             <GridItem image={imgUrl} layout="horizontal" text="标题最多六字" description="说明文字最多八字" />
           </Grid>
-        </div>
-        <div className="grid-demo">
-          <Grid column={3}>
-            <GridItem image={imgUrl} text="标题文字" description="说明文字" />
-            <GridItem image={imgUrl} text="标题文字" description="说明文字" />
-            <GridItem image={imgUrl} text="标题最多六字" description="说明最多六字" />
-          </Grid>
-        </div>
       </TDemoBlock>
+
+      <TDemoBlock title=" " summary="一行三个带说明宫格">
+        <Grid column={3}>
+          <GridItem image={imgUrl} text="标题文字" description="说明文字" />
+          <GridItem image={imgUrl} text="标题文字" description="说明文字" />
+          <GridItem image={imgUrl} text="标题最多六字" description="说明最多六字" />
+        </Grid>
+      </TDemoBlock>
+
       <TDemoBlock title=" " summary="带徽标宫格">
         <div className="grid-demo">
           <Grid>
