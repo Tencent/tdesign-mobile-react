7a766b28c68d2d1a92e10c38a6b51fc23090dda0
Perisiguiendo <1987391831@qq.com>
2022-07-11 10:27:44 +0800

    feat: 新增示例页

---
+++ src/search/_example/color.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import { SearchIcon } from 'tdesign-icons-react';
+import { Search } from 'tdesign-mobile-react';
+
+export default function Color() {
+  const [value, setValue] = useState('');
+
+  return (
+    <Search
+      focus={false}
+      placeholder="搜索预设文案"
+      value={value}
+      onChange={(value) => setValue(value)}
+      action="取消"
+      leftIcon={<SearchIcon color="red" />}
+    />
+  );
+}
