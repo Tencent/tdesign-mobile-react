36972b4b43b7ee487f16f9d72e7a2bb8492a7b61
Perisiguiendo <1987391831@qq.com>
2022-07-11 11:30:35 +0800

    fix: check all export

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,66 @@
-export * from './avatar';
-export * from './back-top';
-export * from './badge';
+/**
+ * 基础组件（除icon，2个）
+ */
 export * from './button';
-export * from './cell';
-export * from './cell-group';
-export * from './checkbox';
-export * from './collapse';
-export * from './count-down';
-export * from './dialog';
 export * from './divider';
-export * from './grid';
-export * from './image';
-export * from './indexes';
-export * from './input';
-export * from './loading';
-export * from './message';
+
+/**
+ * 导航（5个）
+ */
+export * from './steps';
+export * from './tab-bar';
 export * from './navbar';
-export * from './overlay';
+export * from './tabs';
+export * from './indexes';
+
+/**
+ * 输入（11个）
+ */
+export * from './checkbox';
+export * from './input';
 export * from './picker';
-export * from './popup';
-export * from './pull-down-refresh';
 export * from './radio';
 export * from './rate';
 export * from './search';
-export * from './skeleton';
 export * from './slider';
 export * from './stepper';
-export * from './steps';
-export * from './sticky';
-export * from './swipe-cell';
-export * from './swiper';
 export * from './switch';
-export * from './tab-bar';
-export * from './tabs';
-export * from './tag';
 export * from './textarea';
-export * from './toast';
 export * from './upload';
+
+/**
+ * 数据展示（11个）
+ */
+export * from './avatar';
+export * from './badge';
+export * from './cell';
+export * from './cell-group';
+export * from './count-down';
+export * from './grid';
+export * from './image';
+export * from './skeleton';
+export * from './sticky';
+export * from './swiper';
+export * from './swipe-cell';
+export * from './tag';
+
+/**
+ * 消息提醒（7个）
+ */
+export * from './back-top';
+export * from './dialog';
+export * from './loading';
+export * from './message';
+export * from './popup';
+export * from './pull-down-refresh';
+export * from './toast';
+
+/**
+ * 二期组件
+ */
+export * from './collapse';
+
+/**
+ * 辅助功能组件
+ */
+export * from './overlay';
