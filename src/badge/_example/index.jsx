b4b12df6d4091340743195ce82a3bfdd7d73fa19
tealyao <tealyao@tencent.com>
2022-10-26 13:37:11 +0800

    feat: 示例代码改造-badge组件

--- src/badge/_example/index.jsx
+++ src/badge/_example/index.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import TDemoBlock from '../../../site/mobile/components/DemoBlock';
 import TDemoHeader from '../../../site/mobile/components/DemoHeader';
-import NormalDemo from './normal';
-import SingleLineDemo from './single-line';
+import BaseDemo from './base';
 import ButtonDemo from './button';
+import CellDemo from './cell';
 import './style/index.less';
 
 export default function BadgeDemo() {
   return (
     <div className="badge-demo">
-      <TDemoHeader title="Badge 徽标" summary="展示新增内容的提示，用警示红色为主色，包含数字或文字提示内容" />
+      <TDemoHeader title="Badge 徽标" summary="用于告知用户，该区域的状态变化或者待处理任务的数量。" />
 
-      <TDemoBlock title="01 普通徽标">
-        <NormalDemo />
+      <TDemoBlock title="01 类型" summary="徽标主要分红点、数字、文字和角标提醒">
+        <BaseDemo />
       </TDemoBlock>
-      <TDemoBlock title="02 按钮徽标">
+      <TDemoBlock>
         <ButtonDemo />
       </TDemoBlock>
-      <TDemoBlock title="03 单行徽标">
-        <SingleLineDemo />
+      <TDemoBlock>
+        <CellDemo />
       </TDemoBlock>
     </div>
   );
