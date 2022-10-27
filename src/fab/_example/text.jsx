ce40f427c3ed537ca4d4c7c59db84f42a39eea2c
zhongyuan <y243139883@163.com>
2022-10-27 10:09:01 +0800

    docs(fab): update docs and demo

--- src/fab/_example/text.jsx
+++ src/fab/_example/text.jsx
@@ -8,7 +8,13 @@ export default function () {
   };
   return (
     <>
-      <Fab icon={<Icon name="add" />} text="按钮文字" style={{ right: '16px', bottom: '32px' }} onClick={onClick} />
+      <Fab
+        icon={<Icon name="call" />}
+        text="获取手机号"
+        style={{ right: '16px', bottom: '32px' }}
+        buttonProps={{ variant: 'outline' }}
+        onClick={onClick}
+      />
     </>
   );
 }
