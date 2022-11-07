d29198a29fd84ab81699137aa4038debbebd539f
zwkang <kang95630@gmail.com>
2022-11-07 16:22:56 +0800

    test(button): change button example test case to vitest

--- src/button/__tests__/button.test.tsx
+++ src/button/__tests__/button.test.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
-import { testExamples, render } from '@test/utils';
+import { render } from '@test/utils';
 import Button from '../Button';
 
 // 测试组件代码 Example 快照
-testExamples(__dirname);
+// testExamples(__dirname);
+const prefix = 't';
+const name = `${prefix}-button`;
+// const SIZE_CLASSNAMES = {
+//   small: `${prefix}-size-s`,
+//   medium: `${prefix}-size-m`,
+//   large: `${prefix}-size-l`,
+// };
+const TEXT = 'tdesign-mobile-vue';
 
 describe('Button 组件测试', () => {
-  const ButtonText = '按钮组件';
-  test('content', async () => {
-    const { queryByText } = render(<Button content={ButtonText} />);
-    expect(queryByText(ButtonText)).toBeInTheDocument();
+  describe('props', () => {
+    it(': theme', async () => {
+      const { rerender, container } = render(<Button>{TEXT}</Button>);
+      const buttonNode = container.querySelector('.t-button');
+      expect(buttonNode.textContent.trim()).toBe(TEXT);
+      await rerender(<Button theme="danger">{TEXT}</Button>);
+      const dangerBtn = container.querySelector('.t-button');
+      expect(dangerBtn.classList.contains(`${name}-danger`)).toBeTruthy();
+    });
   });
 });
