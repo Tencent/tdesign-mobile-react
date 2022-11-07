b88052243e85305131768f8cadc54e454a6b527c
zwkang <kang95630@gmail.com>
2022-11-07 16:22:17 +0800

    feat: 测试框架切换至 vitest

---
+++ vitest.config.ts
@@ -0,0 +1,33 @@
+import path from 'path';
+import { defineConfig } from 'vitest/config';
+import { InlineConfig } from 'vitest';
+
+// 单元测试相关配置
+const testConfig: InlineConfig = {
+  include:
+    process.env.NODE_ENV === 'test-snap'
+      ? ['test/snap/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']
+      : ['src/**/__tests__/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+  globals: true,
+  environment: 'jsdom',
+  testTimeout: 8000,
+  transformMode: {
+    web: [/\.[jt]sx$/],
+  },
+  coverage: {
+    provider: 'istanbul',
+    reporter: ['text', 'json', 'html'],
+    reportsDirectory: 'test/coverage',
+  },
+};
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      'tdesign-mobile-react/es': path.resolve(__dirname, './src/index.ts'),
+      'tdesign-mobile-react': path.resolve(__dirname, './src/index.ts'),
+      '@test/utils': path.resolve(__dirname, './test/utils'),
+    },
+  },
+  test: testConfig,
+});
