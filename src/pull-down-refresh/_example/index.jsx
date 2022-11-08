4e097ce563dcba7ba96d879c0b15f366e0c9cbea
zhongyuan <y243139883@163.com>
2022-11-08 22:36:34 +0800

    fix(pull-down-refresh): fix cr修复borderRadius不生效的问题

--- src/pull-down-refresh/_example/index.jsx
+++ src/pull-down-refresh/_example/index.jsx
@@ -9,11 +9,12 @@ export default function Demo() {
   const rowCols = [
     1,
     {
-      width: '80%',
+      width: '100px',
     },
     {
-      height: '171px',
-      borderRadius: 16,
+      width: '165.5px',
+      height: '165.5px',
+      borderRadius: '12px',
     },
   ];
   return (
@@ -26,13 +27,12 @@ export default function Demo() {
           />
           <TDemoBlock>
             <div className="tdesign-mobile-block">拖拽该区域演示 顶部下拉刷新</div>
-            <div style={{ display: 'flex' }}>
-              <div className="tdesign-mobile-item">
-                <Skeleton theme="text" rowCol={rowCols} />
-              </div>
-              <div className="tdesign-mobile-item">
-                <Skeleton theme="text" rowCol={rowCols} />
-              </div>
+            <div style={{ display: 'flex', padding: '0 16px 140px 16px' }}>
+              {Array.from(Array(2), (item, key) => (
+                <div className="tdesign-mobile-item" key={key}>
+                  <Skeleton theme="text" rowCol={rowCols} />
+                </div>
+              ))}
             </div>
           </TDemoBlock>
         </>
