b7441dc8affcd6f9d07d6904c51125839c6a87d3
xxxlj <xlj983664@163.com>
2022-11-08 14:44:40 +0800

    feat(progress): update progress docs and demo

--- src/progress/_example/strokeWidth.jsx
+++ src/progress/_example/strokeWidth.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Progress } from 'tdesign-mobile-react';
 import './style/index.less';
 
-export default function NoText() {
+export default function StrokeWidth() {
   return (
     <div className="tdesign-mobile-demo progress">
       <div className="progress-demo progress-demo--margin">
