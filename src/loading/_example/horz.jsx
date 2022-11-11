fc3832c7a4a2b66811f322648ee37f74aee864bd
lockiechen <290015551@qq.com>
2022-11-11 21:23:14 +0800

    docs(Loading): update demo #476

--- src/loading/_example/horz.jsx
+++ src/loading/_example/horz.jsx
@@ -3,12 +3,14 @@ import { Loading } from 'tdesign-mobile-react';
 
 export default function () {
   return (
-    <>
+    <div className="demo-content">
       <Loading text="加载中..." />
+      <div style={{ marginRight: '20px' }} />
       <Loading theme="spinner" text="加载中..." />
+      <div style={{ marginRight: '20px' }} />
       <Loading>
         <span>加载中...</span>
       </Loading>
-    </>
+    </div>
   );
 }
--- src/loading/_example/size.jsx
+++ src/loading/_example/size.jsx
@@ -3,10 +3,10 @@ import { Loading } from 'tdesign-mobile-react';
 
 export default function () {
   return (
-    <>
-      <Loading size="large" text="加载中(大)..." />
-      <Loading size="medium" text="加载中(中)..." />
+    <div className="demo-content demo-content--column demo-content-size">
       <Loading size="small" text="加载中(小)..." />
-    </>
+      <Loading size="medium" text="加载中(中)..." />
+      <Loading size="large" text="加载中(大)..." />
+    </div>
   );
 }
