2cdf0aff35cee65ff33e68391869ff2f32782fc5
Y. <anly_yaw@163.com>
2024-07-19 17:07:00 +0800

    chore: jest changed to vitest (#386)

--- src/button/__tests__/button.test.tsx
+++ src/button/__tests__/button.test.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import { testExamples, render } from '@test/utils';
+import { render } from '@test/utils';
 import Button from '../Button';
-
-// 测试组件代码 Example 快照
-testExamples(__dirname);
 
 describe('Button 组件测试', () => {
   const ButtonText = '按钮组件';
