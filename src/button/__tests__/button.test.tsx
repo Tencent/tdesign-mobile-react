8bc9b3adaff4377427e3eec72b3d56cdc3945b39
黎伟杰 <674416404@qq.com>
2024-07-19 17:53:36 +0800

    chore: bump vitest 2.x

--- src/button/__tests__/button.test.tsx
+++ src/button/__tests__/button.test.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { render } from '@test/utils';
+import { describe, it, expect } from 'vitest';
 import Button from '../Button';
 
 describe('Button 组件测试', () => {
   const ButtonText = '按钮组件';
-  test('content', async () => {
+  it('content', async () => {
     const { queryByText } = render(<Button content={ButtonText} />);
-    expect(queryByText(ButtonText)).toBeInTheDocument();
+    expect(queryByText(ButtonText)).toMatchSnapshot();
   });
 });
