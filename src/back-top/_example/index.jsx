d5c26c258da6df8bb02a2ed356e341156a2b7be1
ssmyaojiayouya <2861884324@qq.com>
2024-08-02 11:02:40 +0800

    feat(back-top): 官网示例代码&&演示示例对齐新视觉稿

--- src/back-top/_example/index.jsx
+++ src/back-top/_example/index.jsx
@@ -1,23 +1,16 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { Skeleton } from 'tdesign-mobile-react';
 import TDemoHeader from '../../../site/mobile/components/DemoHeader';
 import TDemoBlock from '../../../site/mobile/components/DemoBlock';
 import './style/index.less';
-import RoundDemo from './round';
-import HalfRoundDemo from './half-round';
+import BaseDemo from './base';
 
 export default function Base() {
   const [visible, setVisible] = useState(false);
-  const [visible1, setVisible1] = useState(false);
+  const containerRef = useRef(null);
 
   const onClose = () => {
     setVisible(true);
-    setVisible1(false);
-  };
-
-  const onClose1 = () => {
-    setVisible(false);
-    setVisible1(true);
   };
 
   const rowCols = [
@@ -33,20 +26,17 @@ export default function Base() {
   ];
 
   return (
-    <div className="tdesign-mobile-react-demo">
+    <div ref={containerRef} className="tdesign-mobile-react-demo">
       <TDemoHeader
         title="BackTop 返回顶部"
         summary="当页面过长往下滑动是会出现返回顶部的便捷操作，帮助用户快速回到页面顶部"
       />
-      <TDemoBlock title="01 类型" summary="圆型返回顶部">
-        <RoundDemo visible={visible} onClose={onClose} />
-      </TDemoBlock>
-      <TDemoBlock summary="半圆型返回顶部">
-        <HalfRoundDemo visible={visible1} onClose={onClose1} />
+      <TDemoBlock title="形状" summary="">
+        <BaseDemo visible={visible} onClose={onClose} container={() => containerRef.current} />
       </TDemoBlock>
 
       <div className="group">
-        {Array.from(Array(4), (item, key) => (
+        {Array.from(Array(6), (_, key) => (
           <div className="item" key={key}>
             <Skeleton theme="text" rowCol={rowCols} />
           </div>
