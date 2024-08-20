f84683ebf02135e74fb10eece6acf11ada0d0dea
toby <toviyyahxu@gmail.com>
2024-08-20 17:03:49 +0800

    feat(Button): 使用parseTNode包裹TNode

--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -2,6 +2,7 @@ import React, { forwardRef } from 'react';
 import classnames from 'classnames';
 import { LoadingIcon } from 'tdesign-icons-react';
 import useConfig from '../_util/useConfig';
+import parseTNode from '../_util/parseTNode';
 import { TdButtonProps } from './type';
 import noop from '../_util/noop';
 import { buttonDefaultProps } from './defaultProps';
@@ -30,6 +31,8 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
   } = props;
   const { classPrefix } = useConfig();
 
+  const childNode = content || children;
+
   return (
     <button
       ref={ref}
@@ -54,9 +57,9 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
       onClick={!loading && !disabled ? onClick : undefined}
       disabled={disabled || loading}
     >
-      {loading ? <LoadingIcon {...loadingProps} /> : icon}
-      {content || children ? <span className={`${classPrefix}-button__content`}> {content || children}</span> : ''}
-      {suffix}
+      {loading ? <LoadingIcon {...loadingProps} /> : parseTNode(icon)}
+      {childNode && <span className={`${classPrefix}-button__content`}> {parseTNode(childNode)}</span>}
+      {parseTNode(suffix)}
     </button>
   );
 });
