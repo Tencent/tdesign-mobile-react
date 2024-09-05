f3f59d5a57fe8bc764b3accd7692d7831a1f4b0c
epoll-j <epoll@foxmail.com>
2024-09-05 19:44:22 +0800

    refactor(tabs): tabs样式及结构对齐vue

---
+++ src/tabs/defaultProps.ts
@@ -0,0 +1,14 @@
+/**
+ * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
+ * */
+
+import { TdTabsProps } from './type';
+
+export const tabsDefaultProps: TdTabsProps = {
+  bottomLineMode: 'fixed',
+  showBottomLine: true,
+  size: 'medium',
+  theme: 'line',
+  spaceEvenly: true,
+  swipeable: true,
+};
