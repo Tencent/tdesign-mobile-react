6f011612c719615d99e00653a023a2511683a17c
anlyyao <anly_yaw@163.com>
2024-09-18 16:16:32 +0800

    chore: update _common

--- src/empty/index.ts
+++ src/empty/index.ts
@@ -3,6 +3,7 @@ import _Empty from './Empty';
 import './style/index.js';
 
 export type { EmptyProps } from './Empty';
+export * from './type';
 
 export const Empty = _Empty;
 export default Empty;
