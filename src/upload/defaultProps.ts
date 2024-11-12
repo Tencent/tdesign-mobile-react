38bc5658618648fa55fe99d1b74c8c5c704c9750
slatejack <slatexu@gmail.com>
2024-11-12 11:46:34 +0800

    feat(upload): update upload style to v2, alignment vue mobile, update useUpload
    feat #546

---
+++ src/upload/defaultProps.ts
@@ -0,0 +1,20 @@
+/**
+ * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
+ * */
+
+import { TdUploadProps } from './type';
+
+export const uploadDefaultProps: TdUploadProps = {
+  allowUploadDuplicateFile: false,
+  autoUpload: true,
+  disabled: undefined,
+  defaultFiles: [],
+  isBatchUpload: false,
+  max: 0,
+  method: 'POST',
+  multiple: false,
+  name: 'file',
+  uploadAllFilesInOneRequest: false,
+  useMockProgress: true,
+  withCredentials: false,
+};
