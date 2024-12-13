a50b8014123958ce68f1b5dca6b561f82f1a36a9
黎伟杰 <674416404@qq.com>
2024-12-13 15:37:53 +0800

    chore: fix type

--- src/radio/Radio.tsx
+++ src/radio/Radio.tsx
@@ -11,7 +11,7 @@ import useDefaultProps from '../hooks/useDefaultProps';
 import { radioDefaultProps } from './defaultProps';
 
 export interface RadioProps extends TdRadioProps {
-  ref?: Ref<HTMLInputElement>;
+  ref?: Ref<HTMLDivElement>;
 }
 
 export interface RadioContextValue {
@@ -26,7 +26,7 @@ const getLimitRow = (row: number): CSSProperties => ({
   WebkitLineClamp: row,
 });
 
-const Radio = forwardRef((_props: RadioProps, ref: Ref<HTMLInputElement>) => {
+const Radio = forwardRef((_props: RadioProps, ref: Ref<HTMLDivElement>) => {
   const radioClass = usePrefixClass('radio');
   const inputRef = useRef();
   const context = useContext(RadioContext);
@@ -153,6 +153,6 @@ const Radio = forwardRef((_props: RadioProps, ref: Ref<HTMLInputElement>) => {
 });
 
 export default forwardRefWithStatics(
-  (props: RadioProps, ref: Ref<HTMLInputElement>) => <Radio ref={ref} {...props}></Radio>,
+  (props: RadioProps, ref: Ref<HTMLDivElement>) => <Radio ref={ref} {...props}></Radio>,
   { Group: RadioGroup },
 );
