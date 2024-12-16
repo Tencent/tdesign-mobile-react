9958b9a8231d8c8f3102121da7603ab502c77012
xifanTT <514329552@qq.com>
2024-12-16 19:12:49 +0800

    feat: mobile-react同步swiper组件

--- src/swiper/index.ts
+++ src/swiper/index.ts
@@ -1,9 +1,8 @@
-import _Swiper from './Swiper';
-
 import './style/index.js';
+import _Swiper from './Swiper.js';
 
-export type { SwiperProps } from './Swiper';
-export * from './type';
+export type { SwiperProps } from './Swiper.js';
+export * from './type.js';
 
 export const Swiper = _Swiper;
 export default Swiper;
