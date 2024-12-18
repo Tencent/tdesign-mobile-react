26e397c855db0de8026131ada4d5f5a99fb55de2
xifanTT <514329552@qq.com>
2024-12-18 15:40:04 +0800

    fix: 遗落文件

---
+++ src/swiper/index.tsx
@@ -0,0 +1,8 @@
+import './style/index';
+import _Swiper from './Swiper';
+
+export type { SwiperProps } from './Swiper';
+export * from './type';
+
+export const Swiper = _Swiper;
+export default Swiper;
