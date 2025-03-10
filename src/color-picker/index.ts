e2773456e704416c7feece494cdcf294ba7c02ab
Y. <anly_yaw@163.com>
2025-03-10 21:27:35 +0800

    fix(ColorPicker): fix es product errors (#577)

--- src/color-picker/index.ts
+++ src/color-picker/index.ts
@@ -1,9 +1,8 @@
 import _ColorPicker from './ColorPicker';
-
-import './style/index.js';
+import './style';
 
 export type { ColorPickerProps } from './ColorPicker';
-
 export * from './type';
 
 export const ColorPicker = _ColorPicker;
+export default ColorPicker;
