493cd026a24bb47a20b35f78b25c8cbc01ed6365
novlan1 <1576271227@qq.com>
2025-05-26 09:48:06 +0800

    chore: test preview

--- src/image-viewer/image-viewer.tsx
+++ src/image-viewer/image-viewer.tsx
@@ -168,7 +168,7 @@ const ImageViewer: React.FC<ImageViewerProps> = (props) => {
                   imgRefs.current[index] = node;
                 }}
                 style={{
-                  transform: `matrix(${transform.scale}, 0, 0, ${transform.scale}, ${transform.x}, ${transform.y})`,
+                  transform: `matrix(${transform.scale}, 0, 0, ${transform.scale}, ${transform.x}, 0)`,
                   transitionDuration: isTouching ? '0s' : '.3s',
                 }}
                 className={`${imageViewerClass}__img`}
