a7422ec0f38ba04200fa36239f71b423b600e20d
anlyyao <anly_yaw@163.com>
2025-07-08 13:05:12 +0800

    chore: update demo

--- src/table/_example/index.tsx
+++ src/table/_example/index.tsx
@@ -5,6 +5,7 @@ import BaseExample from './base';
 import ScrollExample from './scroll';
 import StripeExample from './stripe';
 import BorderedExample from './bordered';
+import CustomRowStyleExample from './custom-row-style';
 
 export default function Base() {
   return (
@@ -15,15 +16,22 @@ export default function Base() {
       />
       <TDemoBlock title="01 组件类型" summary="基础表格" padding={true}>
         <BaseExample />
+      </TDemoBlock>
 
-        <TDemoHeader title="" summary="横向平铺可滚动表格" padding={true} />
+      <TDemoBlock summary="横向平铺可滚动表格" padding={true}>
         <ScrollExample />
+      </TDemoBlock>
 
-        <TDemoHeader title="" summary="带斑马纹表格样式" padding={true} />
+      <TDemoBlock summary="带斑马纹表格样式" padding={true}>
         <StripeExample />
+      </TDemoBlock>
 
-        <TDemoHeader title="" summary="带边框表格样式" padding={true} />
+      <TDemoBlock summary="带边框表格样式" padding={true}>
         <BorderedExample />
+      </TDemoBlock>
+
+      <TDemoBlock summary="可自定义样式的表格" padding={true}>
+        <CustomRowStyleExample />
       </TDemoBlock>
     </div>
   );
