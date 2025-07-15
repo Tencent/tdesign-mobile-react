24e9e4c2d396b410069f0f2aae1d2d100f2e6399
Slatejack <39983389+slatejack@users.noreply.github.com>
2025-07-15 18:42:41 +0800

    feat(upload): 组件对齐 mobile-vue (#549)
    * feat(upload): update upload style to v2, alignment vue mobile, update useUpload
    
    feat #546
    
    * feat(upload): update upload component, alignment vue mobile
    
    * fix(upload): inject outer className to container className
    
    * chore: update snapshots
    
    * feat(upload): update upload demo, alignment mobile vue
    
    * fix(upload): update globalConfig export from useConfig
    
    * test(upload): update test snapshot
    
    * feat(upload): update upload type
    
    * feat(upload): import imageview, completion upload logic
    
    * fix(upload): fix useUpload props, remove unknown props
    
    * test(upload): update test snapshot
    
    * fix(upload): fix base demo, use message content to object options

--- src/upload/_example/index.jsx
+++ src/upload/_example/index.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import TDemoHeader from '../../../site/mobile/components/DemoHeader';
 import TDemoBlock from '../../../site/mobile/components/DemoBlock';
-import Base from './base';
-import Multiple from './multiple';
+import BaseDemo from './base';
+import MultipleDemo from './multiple';
+import StatusDemo from './status';
+import CustomDemo from './custom';
 
 export default function () {
   return (
     <div className="tdesign-mobile-demo" style={{ marginBottom: 16 }}>
-      <TDemoHeader title="Upload 上传" summary="用于相册读取或拉起拍照的图片上传功能。" />
+      <TDemoHeader title="Upload上传" summary="用于相册读取或拉起拍照的图片上传功能。" />
       <TDemoBlock title="01 类型" summary="单选上传图片">
-        <Base />
+        <BaseDemo />
       </TDemoBlock>
-      <TDemoBlock title="" summary="多选上传图片">
-        <Multiple />
+      <TDemoBlock summary="多选上传图片">
+        <MultipleDemo />
+      </TDemoBlock>
+      <TDemoBlock title="02 组件状态" summary="加载状态">
+        <StatusDemo />
+      </TDemoBlock>
+      <TDemoBlock title="03 样式自定义" summary="单选上传">
+        <CustomDemo />
       </TDemoBlock>
     </div>
   );
---
+++ src/upload/defaultProps.ts
@@ -0,0 +1,20 @@
+/**
+ * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
+ * */
+
+import { TdUploadProps } from './type';
+
+export const uploadDefaultProps: TdUploadProps = {
+  addBtn: true,
+  allowUploadDuplicateFile: false,
+  autoUpload: true,
+  disabled: undefined,
+  defaultFiles: [],
+  max: 0,
+  method: 'POST',
+  multiple: false,
+  preview: true,
+  removeBtn: true,
+  useMockProgress: true,
+  withCredentials: false,
+};
