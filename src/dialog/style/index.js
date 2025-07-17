0d77d53fa6adab94f39bb96eb24282a7b8b199b2
Y. <anly_yaw@163.com>
2025-07-17 10:43:14 +0800

    chore: update the style file import path (#683)
    * chore: update the style file import path
    
    * chore: force omission of leading decimal point
    
    * chore: update common
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/dialog/style/index.js
+++ src/dialog/style/index.js
@@ -1,2 +1,1 @@
-/* eslint-disable import/no-relative-packages */
-import '../../_common/style/mobile/components/dialog/v2/_index.less';
+import '../../_common/style/mobile/components/dialog/_index.less';
