accf42636aa3c4ca8e64e94bb796998db6f783c4
Boomkaa <173308582@qq.com>
2025-08-07 14:15:45 +0800

    test: add test cases of button

--- src/button/_example/size.tsx
+++ src/button/_example/size.tsx
@@ -5,14 +5,14 @@ export default function () {
   return (
     <div className="row section-size">
       <Button size="large" theme="primary">
-        按钮 48
+        按钮48
       </Button>
-      <Button theme="primary">按钮 40</Button>
+      <Button theme="primary">按钮40</Button>
       <Button size="small" theme="primary">
-        按钮 32
+        按钮32
       </Button>
       <Button size="extra-small" theme="primary">
-        按钮 28
+        按钮28
       </Button>
     </div>
   );
