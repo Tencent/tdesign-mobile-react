ee8dc6872e6e789f8730b798a9106d45bf01881b
JustGenius-s <53108645+JustGenius-s@users.noreply.github.com>
2025-09-03 12:50:18 +0800

    test(pull-down-refresh): add pull-down-refresh test (#774)
    * test(pull-down-refresh): add pull-down-refresh test
    
    * test(pull-down-refresh): improve coverage
    
    * test(pull-down-refresh): improve code and add coverage
    
    * test(pull-down-refresh): improve hooks test
    
    * test(pull-down-refresh): improve test coverage
    
    * fix(PullDownRefresh): fix change event always being triggered twice
    
    * chore: update demo
    
    ---------
    
    Co-authored-by: anlyyao <anly_yaw@163.com>

--- src/pull-down-refresh/_example/index.tsx
+++ src/pull-down-refresh/_example/index.tsx
@@ -7,15 +7,15 @@ import './style/index.less';
 
 export default function Demo() {
   const rowCols = [
-    1,
-    {
-      width: '100px',
-    },
     {
       width: '165.5px',
       height: '165.5px',
       borderRadius: '12px',
     },
+    {
+      width: '100px',
+    },
+    1,
   ];
   return (
     <div className="tdesign-mobile-demo">
@@ -27,11 +27,9 @@ export default function Demo() {
           />
           <TDemoBlock>
             <div className="tdesign-mobile-block">拖拽该区域演示 顶部下拉刷新</div>
-            <div style={{ display: 'flex', padding: '0 16px 140px 16px' }}>
-              {Array.from(Array(2), (_, key) => (
-                <div className="tdesign-mobile-item" key={key}>
-                  <Skeleton theme="text" rowCol={rowCols} />
-                </div>
+            <div className="group">
+              {Array.from(Array(8), (_, key) => (
+                <Skeleton className="item" key={key} theme="text" rowCol={rowCols} />
               ))}
             </div>
           </TDemoBlock>
