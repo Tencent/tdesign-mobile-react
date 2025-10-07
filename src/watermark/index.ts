724fbf966dd529cf0747709e019bcb32e6ad9afb
Wesley <985189328@qq.com>
2025-10-08 00:05:56 +0800

    feat: watermark

---
+++ src/watermark/index.ts
@@ -0,0 +1,10 @@
+import _Watermark from './Watermark';
+
+import './style/index.js';
+
+export type { WatermarkProps } from './Watermark';
+
+export * from './type';
+
+export const Watermark = _Watermark;
+export default Watermark;
