7067b15470cab9d2c766818dc8cceaaa72be02b9
anlyyao <anly_yaw@163.com>
2026-02-11 15:25:49 +0800

    feat(Tabs): update demo

--- src/tabs/_example/icon.tsx
+++ src/tabs/_example/icon.tsx
@@ -1,33 +1,17 @@
 import React from 'react';
 import { Tabs, TabPanel } from 'tdesign-mobile-react';
-import { IconFont } from 'tdesign-icons-react';
+import { AppIcon } from 'tdesign-icons-react';
 
 export default () => (
   <div>
     <Tabs defaultValue={'first'}>
-      <TabPanel
-        value={'first'}
-        label={
-          <div className="label-content">
-            <IconFont name="app" size="large"></IconFont>
-            <span>选项</span>
-          </div>
-        }
-      ></TabPanel>
-      <TabPanel
-        value={'second'}
-        label={
-          <div className="label-content">
-            <IconFont name="app" size="large"></IconFont>
-            <span>选项</span>
-          </div>
-        }
-      ></TabPanel>
+      <TabPanel value={'first'} label={'选项'} icon={<AppIcon />}></TabPanel>
+      <TabPanel value={'second'} label={'选项'} icon={<AppIcon />}></TabPanel>
       <TabPanel
         value={'third'}
         label={
           <div className="label-content">
-            <IconFont name="app" size="large"></IconFont>
+            <AppIcon size="large" />
             <span>选项</span>
           </div>
         }
