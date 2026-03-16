aa86384df9b861c21ef76a6ea7a2c2eafbfb2151
Rylan <rylanbot@qq.com>
2026-03-16 18:09:48 +0800

    fix(Slider): change `<text>` to `<div>` (#889)

--- src/count-down/_example/base.tsx
+++ src/count-down/_example/base.tsx
@@ -7,42 +7,42 @@ export default function BaseCountDown() {
   return (
     <>
       <div className="demo-count-down">
-        <text className="demo-count-down-desc"> 时分秒 </text>
+        <div className="demo-count-down-desc"> 时分秒 </div>
         <div className="demo-count-down-content">
           <CountDown time={time} />
         </div>
       </div>
 
       <div className="demo-count-down">
-        <text className="demo-count-down-desc"> 带毫秒 </text>
+        <div className="demo-count-down-desc"> 带毫秒 </div>
         <div className="demo-count-down-content">
           <CountDown time={time} millisecond />
         </div>
       </div>
 
       <div className="demo-count-down">
-        <text className="demo-count-down-desc"> 带方形底 </text>
+        <div className="demo-count-down-desc"> 带方形底 </div>
         <div className="demo-count-down-content">
           <CountDown content="default" time={time} theme="square" />
         </div>
       </div>
 
       <div className="demo-count-down">
-        <text className="demo-count-down-desc"> 带圆形底 </text>
+        <div className="demo-count-down-desc"> 带圆形底 </div>
         <div className="demo-count-down-content">
           <CountDown time={time} theme="round" />
         </div>
       </div>
 
       <div className="demo-count-down">
-        <text className="demo-count-down-desc"> 带单位 </text>
+        <div className="demo-count-down-desc"> 带单位 </div>
         <div className="demo-count-down-content">
           <CountDown time={time} split-with-unit theme="round" />
         </div>
       </div>
 
       <div className="demo-count-down">
-        <text className="demo-count-down-desc"> 无底色带单位 </text>
+        <div className="demo-count-down-desc"> 无底色带单位 </div>
         <div className="demo-count-down-content">
           <CountDown className="custom" time={time} split-with-unit />
         </div>
