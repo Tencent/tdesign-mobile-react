410cd398fa2b0a63cebd3cd5551d8e8f129e4f2c
Y. <anly_yaw@163.com>
2026-06-29 14:47:19 +0800

    fix(PullDownRefresh): fix refresh event always being triggered twice

--- src/pull-down-refresh/PullDownRefresh.tsx
+++ src/pull-down-refresh/PullDownRefresh.tsx
@@ -124,7 +124,7 @@ const PullDownRefresh: React.FC<PullDownRefreshProps> = (originProps) => {
         new Promise((resolve) => {
           timeoutTid = setTimeout(() => {
             resolve(timeoutId);
-            onTimeout();
+            onTimeout?.();
           }, refreshTimeout);
         }),
       ]);
@@ -141,7 +141,7 @@ const PullDownRefresh: React.FC<PullDownRefreshProps> = (originProps) => {
     }
   };
 
-  const statusText = getStatusText(status, loadingTexts.length ? loadingTexts : (t(locale.loadingTexts) as string[]));
+  const statusText = getStatusText(status, loadingTexts?.length ? loadingTexts : (t(locale.loadingTexts) as string[]));
   let statusNode: React.ReactNode = <div className={`${name}__text`}>{statusText}</div>;
   if (status === PullStatusEnum.loading) {
     statusNode = <Loading text={statusText} size="24px" {...loadingProps} />;
@@ -194,7 +194,6 @@ const PullDownRefresh: React.FC<PullDownRefreshProps> = (originProps) => {
 
     if (status === PullStatusEnum.loosing) {
       doRefresh();
-      props.onRefresh?.();
     } else {
       setDistance(0);
     }
