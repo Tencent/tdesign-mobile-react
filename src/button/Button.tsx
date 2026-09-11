1ace24e7ca20da530899e5588b92cef07409b83d
Y. <anly_yaw@163.com>
2026-09-11 14:45:37 +0800

    feat(Button): add tag href and form props

--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -9,8 +9,9 @@ import useDefaultProps from '../hooks/useDefaultProps';
 import useHover from '../hooks/useHover';
 
 export interface ButtonProps
-  extends TdButtonProps,
-    Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'content' | 'children'> {}
+  extends
+    TdButtonProps,
+    Omit<React.AllHTMLAttributes<HTMLElement>, 'content' | 'children' | 'size' | 'type' | 'shape'> {}
 
 const Button: React.FC<ButtonProps> = (originProps) => {
   const props = useDefaultProps(originProps, buttonDefaultProps);
@@ -21,17 +22,21 @@ const Button: React.FC<ButtonProps> = (originProps) => {
     children,
     content,
     disabled,
+    form,
     ghost,
+    href,
     icon,
     suffix,
     loading,
     shape,
     size,
+    tag,
     theme,
     type,
     variant,
     onClick,
     loadingProps,
+    ...otherProps
   } = props;
   const buttonClass = usePrefixClass('button');
   const childNode = content || children;
@@ -39,11 +44,37 @@ const Button: React.FC<ButtonProps> = (originProps) => {
   const hoverDisabled = useMemo(() => disabled || loading, [disabled, loading]);
   const ref = useHover({ className: `${buttonClass}--hover`, disabled: hoverDisabled });
 
-  return (
-    <button
-      ref={ref}
-      type={type}
-      className={classnames(
+  const renderTag = useMemo(() => (!tag && href ? 'a' : tag || 'button'), [tag, href]);
+  const isNativeButton = useMemo(() => renderTag === 'button', [renderTag]);
+  const isAriaDisabled = useMemo(() => loading || (!isNativeButton && disabled), [loading, isNativeButton, disabled]);
+  const shouldInterceptClick = useMemo(
+    () => loading || (!isNativeButton && disabled),
+    [loading, isNativeButton, disabled],
+  );
+
+  const tabIndex = useMemo(() => {
+    if (!disabled) return 0;
+    return isNativeButton ? undefined : -1;
+  }, [disabled, isNativeButton]);
+
+  const handleDisabledClick = (event: React.MouseEvent<HTMLElement>) => {
+    event.preventDefault();
+    event.stopPropagation();
+  };
+
+  return React.createElement(
+    renderTag,
+    {
+      ...otherProps,
+      ref,
+      role: isNativeButton ? undefined : 'button',
+      type: isNativeButton ? type : undefined,
+      disabled: isNativeButton ? disabled || loading : undefined,
+      'aria-disabled': isAriaDisabled || undefined,
+      href: href || undefined,
+      tabIndex,
+      form: isNativeButton ? form : undefined,
+      className: classnames(
         [
           `${buttonClass}`,
           `${buttonClass}--size-${size}`,
@@ -58,15 +89,15 @@ const Button: React.FC<ButtonProps> = (originProps) => {
           [`${buttonClass}--disabled`]: disabled,
           [`${buttonClass}--block`]: block,
         },
-      )}
-      style={style}
-      onClick={!loading && !disabled ? onClick : undefined}
-      disabled={disabled || loading}
-    >
+      ),
+      style,
+      onClick: shouldInterceptClick ? handleDisabledClick : onClick,
+    },
+    <>
       {loading ? <TLoading inheritColor {...loadingProps} /> : parseTNode(icon)}
       {childNode && <span className={`${buttonClass}__content`}> {parseTNode(childNode)}</span>}
       {parseTNode(suffix)}
-    </button>
+    </>,
   );
 };
 
