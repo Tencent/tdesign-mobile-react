aa176e870cfabb3d894947685e6d4b2e0907dc97
黎伟杰 <674416404@qq.com>
2026-09-11 09:44:21 +0800

    docs(site): 更新移动端组件库描述

--- site/web/plugins/generate-llms/index.js
+++ site/web/plugins/generate-llms/index.js
@@ -102,7 +102,7 @@ export default function generateLlmsPlugin() {
         platform: 'mobile',
         parseComponentDoc,
         siteTitle: 'TDesign Mobile React',
-        siteDescription: 'TDesign Mobile React 组件库的 LLM 友好文档索引。',
+        siteDescription: 'TDesign 适配移动端的组件库，适合在 React 技术栈项目中使用。',
         siteBaseUrl: config.base,
       });
     },
