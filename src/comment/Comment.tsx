5f78e8ecb85f6bd4f14696fe68f93a514e33dae5
slatejack <slatexu@gmail.com>
2026-09-23 18:19:29 +0800

    feat(comment): add onActions and onFolds operations

--- src/comment/Comment.tsx
+++ src/comment/Comment.tsx
@@ -1,11 +1,11 @@
 import React, { ReactNode, useRef, useState } from 'react';
 import { isObject } from 'lodash-es';
 import { ChevronDownSIcon, ChevronUpSIcon } from 'tdesign-icons-react';
-import { CommentFold, CommentFoldState, TdCommentProps } from './type';
+import { CommentActionItem, CommentFold, CommentFoldState, TdCommentProps } from './type';
 import { usePrefixClass } from '../hooks/useClass';
 import Avatar, { AvatarProps } from '../avatar';
 import parseTNode, { parseContentTNode } from '../_util/parseTNode';
-import { StyledProps } from '../common';
+import { StyledProps, TNode } from '../common';
 
 export interface CommentProps extends TdCommentProps, StyledProps {}
 
@@ -20,24 +20,48 @@ const DEFAULT_COMMENT_FOLDS: CommentFold = {
   },
 };
 const Comment: React.FC<TdCommentProps> = (props) => {
-  const { actions, author, avatar, content, datetime, folds, reply, children } = props;
+  const { actions, author, avatar, content, datetime, folds, reply, children, onActions, onFolds } = props;
   const commentFolds = folds || DEFAULT_COMMENT_FOLDS;
   const [curFoldsState, setCurFoldsState] = useState<CommentFoldState>('collapsed');
   const foldSteps = useRef(0);
   const rootClassName = usePrefixClass('comment');
 
-  const handleClickFold = (isExpand: boolean) => {
-    if (isExpand) {
-      foldSteps.current += 1;
-    } else {
-      foldSteps.current -= 1;
-    }
-    if (foldSteps.current === 0) {
+  const handleClickActions = (event: MouseEvent, action: CommentActionItem | TNode) => {
+    onActions?.({ action, e: event });
+  };
+  const handleClickFold = (event: MouseEvent, isExpand: boolean) => {
+    if (!isExpand) {
+      // 收起点击后收起全部仅展示 1 条回复
       setCurFoldsState('collapsed');
-    } else if (foldSteps.current === commentFolds.step - 1) {
+      foldSteps.current = 0;
+      onFolds?.({
+        fold: {
+          ...commentFolds,
+          state: 'collapsed',
+        },
+        e: event,
+      });
+      return;
+    }
+    foldSteps.current += 1;
+    if (foldSteps.current === commentFolds.step - 1) {
       setCurFoldsState('expanded');
+      onFolds?.({
+        fold: {
+          ...commentFolds,
+          state: 'expanded',
+        },
+        e: event,
+      });
     } else {
       setCurFoldsState('partial');
+      onFolds?.({
+        fold: {
+          ...commentFolds,
+          state: 'partial',
+        },
+        e: event,
+      });
     }
   };
 
@@ -56,26 +80,36 @@ const Comment: React.FC<TdCommentProps> = (props) => {
         return null;
       }
       if (!Array.isArray(actions)) {
-        return parseTNode(actions);
+        return (
+          <div onClick={(e) => handleClickActions(e as unknown as MouseEvent, actions)}>{parseTNode(actions)}</div>
+        );
       }
       const renderLeftPlacement = () =>
         actions
           .filter((item) => item.placement === 'start')
-          .map((item) => parseContentTNode(item.content, { disabled: item.disabled }));
+          .map((item) => (
+            <div key={item.key} onClick={(e) => handleClickActions(e as unknown as MouseEvent, item)}>
+              {parseContentTNode(item.content, { disabled: item.disabled })}
+            </div>
+          ));
       const renderRightPlacement = () =>
         actions
           .filter((item) => item.placement === 'end')
-          .map((item) => parseContentTNode(item.content, { disabled: item.disabled }));
+          .map((item) => (
+            <div key={item.key} onClick={(e) => handleClickActions(e as unknown as MouseEvent, item)}>
+              {parseContentTNode(item.content, { disabled: item.disabled })}
+            </div>
+          ));
       return (
-        <div className={`${rootClassName}__bottom`}>
-          {renderLeftPlacement()}
-          {renderRightPlacement()}
-        </div>
+        <>
+          <div className={`${rootClassName}-block`}>{renderLeftPlacement()}</div>
+          <div className={`${rootClassName}-block`}>{renderRightPlacement()}</div>
+        </>
       );
     };
     return (
       <div className={`${rootClassName}__bottom`}>
-        <div className={`${rootClassName}__datetime`}>{parseTNode(datetime)}</div>
+        <div className={`${rootClassName}__date`}>{parseTNode(datetime)}</div>
         <div className={`${rootClassName}__actions`}>{renderActions()}</div>
       </div>
     );
@@ -91,7 +125,10 @@ const Comment: React.FC<TdCommentProps> = (props) => {
       if (curFoldsState === 'collapsed') {
         // 全收起
         return (
-          <div className={`${rootClassName}__folds-item`} onClick={() => handleClickFold(true)}>
+          <div
+            className={`${rootClassName}__folds-item`}
+            onClick={(e) => handleClickFold(e as unknown as MouseEvent, true)}
+          >
             {commentFolds.content.collapsed as ReactNode}
             <ChevronDownSIcon />
           </div>
@@ -101,11 +138,17 @@ const Comment: React.FC<TdCommentProps> = (props) => {
         // 半展开
         return (
           <>
-            <div className={`${rootClassName}__folds-item`} onClick={() => handleClickFold(true)}>
+            <div
+              className={`${rootClassName}__folds-item`}
+              onClick={(e) => handleClickFold(e as unknown as MouseEvent, true)}
+            >
               {commentFolds.content.partial[0] as ReactNode}
               <ChevronDownSIcon />
             </div>
-            <div className={`${rootClassName}__folds-item`} onClick={() => handleClickFold(false)}>
+            <div
+              className={`${rootClassName}__folds-item`}
+              onClick={(e) => handleClickFold(e as unknown as MouseEvent, false)}
+            >
               {commentFolds.content.partial[1] as ReactNode}
               <ChevronUpSIcon />
             </div>
@@ -114,13 +157,16 @@ const Comment: React.FC<TdCommentProps> = (props) => {
       }
       // 全展开
       return (
-        <div className={`${rootClassName}__folds-item`} onClick={() => handleClickFold(false)}>
+        <div
+          className={`${rootClassName}__folds-item`}
+          onClick={(e) => handleClickFold(e as unknown as MouseEvent, false)}
+        >
           {commentFolds.content.expanded as ReactNode}
           <ChevronUpSIcon />
         </div>
       );
     };
-    if (!commentFolds.step) {
+    if (!commentFolds.total || commentFolds.total <= 1) {
       return null;
     }
     return <div className={`${rootClassName}__folds`}>{renderFoldsContent()}</div>;
@@ -137,7 +183,6 @@ const Comment: React.FC<TdCommentProps> = (props) => {
         </div>
       </div>
       {renderReply()}
-      {renderFolds()}
       {renderFolds()}
     </div>
   );
